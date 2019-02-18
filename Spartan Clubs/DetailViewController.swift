c678c66b7b1176b83a4feb22faa08171639fcb49
Alam Figueroa Aguilar <a_lam_1000@hotmail.com>
2019-02-18 14:39:48 -0800

    Initial Commit

---
+++ Spartan Clubs/DetailViewController.swift
@@ -0,0 +1,40 @@
+//
+//  DetailViewController.swift
+//  Spartan Clubs
+//
+//  Created by Alam Figueroa Aguilar on 2/18/19.
+//  Copyright © 2019 Alam Figueroa Aguilar. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    @IBOutlet weak var detailDescriptionLabel: UILabel!
+
+
+    func configureView() {
+        // Update the user interface for the detail item.
+        if let detail = detailItem {
+            if let label = detailDescriptionLabel {
+                label.text = detail.description
+            }
+        }
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        configureView()
+    }
+
+    var detailItem: NSDate? {
+        didSet {
+            // Update the view.
+            configureView()
+        }
+    }
+
+
+}
+
